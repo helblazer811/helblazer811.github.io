ef50e89c2b4e462882e7fcdcfe0f4a65a1032a11
Alec Helbling <alechelbling1@gmail.com>
2026-03-10 14:48:14 -0400

    Update arboretum

---
+++ arboretum/_app/immutable/chunks/BDIZBMpQ.js
@@ -0,0 +1,1 @@
+import{i as a}from"./CsoqBw9E.js";a();
---
+++ arboretum/_app/immutable/chunks/BzzQlu2c.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1ldbmad)==null?void 0:s.base)??"/arboretum";var a;const t=((a=globalThis.__sveltekit_1ldbmad)==null?void 0:a.assets)??e??"";export{t as a,e as b};
