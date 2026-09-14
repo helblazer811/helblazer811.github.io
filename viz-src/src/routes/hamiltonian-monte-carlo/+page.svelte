29ad6dfe7ed8b55a5b5655fb96679a97eb57c1a6
Anonymous <alechelbling1@gmail.com>
2026-09-13 22:45:09 -0400

    Add MCMC comparison to HMC page

--- viz-src/src/routes/hamiltonian-monte-carlo/+page.svelte
+++ viz-src/src/routes/hamiltonian-monte-carlo/+page.svelte
@@ -2,6 +2,7 @@
 	import { base } from '$app/paths';
 	import PageHeader from '$lib/PageHeader.svelte';
 	import VideoExplainer from '$lib/VideoExplainer.svelte';
+	import HmcVsRandomWalkMcmc from './comparison/HmcVsRandomWalkMcmc.svelte';
 </script>
 
 <svelte:head>
@@ -16,6 +17,8 @@
 	dynamics. These structured trajectories can travel much farther through a distribution than a
 	random-walk proposal.
 </p>
+
+<HmcVsRandomWalkMcmc />
 
 <VideoExplainer
 	src={`${base}/hamiltonian-monte-carlo-16x9.mp4`}
